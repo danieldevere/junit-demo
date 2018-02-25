fba674cd2da922747ec06f9f9e873c15685695bb
Dan DeVere <danieldevere285@gmail.com>
2018-02-25 00:54:32 -0600

    added hello world and test

--- src/test/java/com/maroons/junitdemo/JunitDemoApplicationTests.java
+++ src/test/java/com/maroons/junitdemo/JunitDemoApplicationTests.java
@@ -1,16 +1,24 @@
 package com.maroons.junitdemo;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.maroons.junitdemo.embedded.EmbeddedMysqlManager;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class JunitDemoApplicationTests {
 	protected EmbeddedMysqlManager mysqlManager = EmbeddedMysqlManager.getInstance();
+	
+	@Autowired
+	TestRestTemplate template;
 
 	@Test
 	public void contextLoads() {
@@ -18,8 +26,9 @@ public class JunitDemoApplicationTests {
 	}
 	
 	@Test
-	public void loadDatabase() {
-		mysqlManager.reloadSchema();
+	public void testHello() {
+		String response = template.getForEntity("/hello", String.class).getBody();
+		assertThat(response).isEqualTo("Hello World!");
 	}
 
 }
